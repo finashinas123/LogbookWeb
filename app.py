af5f979c48f0496241c13058059f8b4bb4fffd13
finashinas123 <finashinas@gmail.com>
2025-05-08 19:00:16 +0300

    Fix alignment of radio buttons

--- app.py
+++ app.py
@@ -101,4 +101,6 @@ def submit():
     return send_file(pdf_path, as_attachment=True)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port or default to 5000 locally
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
